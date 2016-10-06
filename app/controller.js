96fe04d6a21d18ac59dd26ca5fbbb86555855378
Jospeh Kalb <kalb.joseph@gmail.com>
2016-10-06 15:12:01 -0700

    second commit

--- app/controller.js
+++ app/controller.js
@@ -5,15 +5,16 @@
         .module('weatherModule')
         .controller('weatherController', weatherController);
 
-    weatherController.$inject = ['$http', 'weatherFactory'];
+    weatherController.$inject = ['$http', 'weatherFactory', 'toastr'];
     
     /* @ngInject */
-    function weatherController($http, weatherFactory) {
+    // start out with making the controller
+    function weatherController($http, weatherFactory, toastr) {
         var vm = this;
         vm.title = 'weatherController';
-        vm.history = [{}];
+        vm.history = [{}]; // have a history for all the previous inputs
         vm.placeInput;
-        vm.name = '';
+        vm.name = ''; // use a blank as a starting point to hide the class="panel-body"
         vm.temp;
         vm.desc;
         vm.lat;
@@ -24,37 +25,42 @@
         vm.high;
         vm.speed;
         vm.dt;
-        vm.icon = '04n';
+        vm.icon = '10d'; // give a base line to make the <img> not throw an error
         
-
         activate();
 
         ////////////////
 
         function activate() {
         }
-
+        // creating the function that will grab all of the user data
         vm.showWeather = function(newPlace){
-        	weatherFactory.getWeather(newPlace).then(function(result){
-        		vm.name = result.name;
-        		vm.desc = result.weather[0].description;
-        		vm.temp = Math.round(result.main.temp * 9/5 - 459.67);
-        		vm.lat = result.coord.lat;
-        		vm.long = result.coord.lon;
-        		vm.pressure = result.main.pressure;
-        		vm.humid = result.main.humidity;
-        		vm.low = Math.round(result.main.temp_min * 9/5 - 459.67);
-        		vm.high = Math.round(result.main.temp_max * 9/5 - 459.67);
-        		vm.speed = result.wind.speed;
-        		vm.dt = Date(result.dt * 1000).replace(' GMT-0700 (PDT)', '');
-        		vm.icon = result.weather[0].icon;
-        		// add the new place to the history array
-        		vm.history.push({
-        		'place': vm.name,
-        		'dt' : vm.dt
-        		});
-        	});
-        }  
-    }
+        	weatherFactory.getWeather(newPlace).then(
+                function(result){
+        		  vm.name = result.name;
+        		  vm.desc = result.weather[0].description; // why is .weather an array?... 
+        		  vm.temp = result.main.temp;
+        		  vm.lat = result.coord.lat;
+        		  vm.long = result.coord.lon;
+        		  vm.pressure = result.main.pressure;
+        		  vm.humid = result.main.humidity;
+        		  vm.low =result.main.temp_min;
+        		  vm.high =result.main.temp_max;
+        		  vm.speed = result.wind.speed;
+        		  vm.dt = Date(result.dt * 1000).replace(' GMT-0700 (PDT)', ''); // convert to human readable
+        		  vm.icon = result.weather[0].icon;
+        		  // add the new place to the history array
+        		  vm.history.push({
+        		      'place': vm.name,
+        		      'dt' : vm.dt
+        		  });
+                  toastr.success('It works!');
+                }, 
+                function(error){
+                    toastr.error('Oh NOOOO Controller error: ' + error.data);
+                }
+            )
+        }
+    }    
 })();
 
--- app/factory.js
+++ app/factory.js
@@ -3,21 +3,37 @@
     angular
         .module('weatherModule')
         .factory('weatherFactory', weatherFactory);
-    weatherFactory.$inject = ['$http'];
+    weatherFactory.$inject = ['$http', '$q'];
     /* @ngInject */
-    function weatherFactory($http) {
+    function weatherFactory($http, $q) {
         var service = {
             getWeather: getWeather
         };
         return service;
         ////////////////
         function getWeather(place) {
-        	return $http({
+            // using toastr
+            var defer = $q.defer();
+
+        	$http({ // grab the weather api
         		method: 'GET',
-        		url: 'http://api.openweathermap.org/data/2.5/weather?q=' + place +  '&APPID=7b2cfaaa74568f2cc3165460db7eba8c'
-        	}).then(function(response) {
-        		return response.data;
+        		url: 'http://api.openweathermap.org/data/2.5/weather',
+                params: {
+                    APPID: '7b2cfaaa74568f2cc3165460db7eba8c', 
+                    q: place, 
+                    units: 'imperial'
+                }
+        	}).then(function(result) { // make sure the data is good through toastr
+                if(typeof result.data === 'object') {
+        	       defer.resolve(result.data);
+                } else {
+                    defer.reject('oh no factory problem!!!');
+                }
+            },
+            function(error) {
+                defer.reject(error);
         	});
+            return defer.promise;
         }
     }
 })();
